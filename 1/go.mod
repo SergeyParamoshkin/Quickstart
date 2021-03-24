74557a642e1c9285d1b59b835d60cad4193a0d39
Sergey <serg3091@gmail.com>
2021-03-24 19:35:44 +0300

    add version add workout :)

---
+++ 1/go.mod
@@ -0,0 +1,3 @@
+module github.com/SergeyParamoshkin/Quickstart/1
+
+go 1.16
--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/SergeyParamoshkin/Quickstart/version"
+)
 
 func main() {
-	fmt.Println("Hello Golang World")
+	// nolint
+	fmt.Println("Hello Golang World", version.VERSION)
 }
